0169c64abaaa51b424101d31f4751a3de0a12405
Andrea Perez Manzaneque <andperman@Ordenador-portatil-de-Andrea.local>
2024-11-28 14:50:25 +0100

    Card/información

--- frontReact/src/App.jsx
+++ frontReact/src/App.jsx
@@ -1,14 +1,19 @@
-// src/App.jsx
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { MapProvider } from './context/MapContext';
 import MapPage from './components/Main/MapPage';
-
+import Card from './components/Main/Card';
 
 function App() {
   return (
-    <MapProvider>
-      <MapPage />
-    </MapProvider>
+    <Router>
+      <MapProvider>
+        <Routes>
+          <Route path="/" element={<MapPage />} />
+          <Route path="/location/:id" element={<Card />} />
+        </Routes>
+      </MapProvider>
+    </Router>
   );
 }
 
--- frontReact/src/components/Main/MapPage/MapView/MapView.jsx
+++ frontReact/src/components/Main/MapPage/MapView/MapView.jsx
@@ -1,9 +1,11 @@
 import React, { useContext } from 'react';
 import { Marker, Popup } from 'react-leaflet';
+import { useNavigate } from 'react-router-dom';
 import { MapContext } from '../../../../context/MapContext';
 
 const MapView = () => {
   const { locations } = useContext(MapContext);
+  const navigate = useNavigate();
 
   if (!locations.length) return null;
 
@@ -14,7 +16,13 @@ const MapView = () => {
           <Popup>
             <div>
               <img src={location.image} alt={location.name} style={{ width: '100px' }} />
-              <p>{location.title}</p>
+              {/* Redirige al usuario al hacer clic en el título */}
+              <p
+                onClick={() => navigate(`/location/${index}`)} // Usa el índice como identificador
+                style={{ color: 'blue', textDecoration: 'underline', cursor: 'pointer' }}
+              >
+                {location.title}
+              </p>
             </div>
           </Popup>
         </Marker>
